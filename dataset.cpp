9ba947bcd293a3b7bde7691972c8c40339054671
Jon7272 <sc23jo2@leeds.ac.uk>
2024-11-29 14:55:59 +0000

    testing commit

---
+++ dataset.cpp
@@ -0,0 +1,37 @@
+#include <stdexcept>
+#include <algorithm>
+#include <numeric>
+#include "dataset.hpp"
+#include "csv.hpp"
+
+using namespace std;
+// jon s
+void WaterDataset::loadData(const string& filename)
+{
+    csv::CSVReader reader(filename);
+
+    data.clear();
+
+    for (const auto& row: reader) {
+        Water water{
+            row["sample.samplingPoint.label"].get<std::string>(),
+            row["sample.sampleDateTime"].get<std::string>(),
+            row["determinand.label"].get<std::string>(),
+            row["result"].get<double>(),
+            row["determinand.unit.label"].get<std::string>(),
+            row["sample.sampledMaterialType.label"].get<std::string>(),
+            row["sample.isComplianceSample"].get<bool>(),
+            row["sample.purpose.label"].get<std::string>(),
+            row["sample.samplingPoint.easting"].get<int>(),
+            row["sample.samplingPoint.northing"].get<int>()
+        };
+        data.push_back(row);
+    }
+}
+
+void WaterDataset::checkDataExists() const
+{
+    if (size() == 0) {
+        throw std::runtime_error("Dataset is empty!");
+    }
+}
---
+++ dataset.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+#include "quake.hpp"
+
+class WaterDataset
+{
+    public:
+        WaterDataset() {}
+        WaterDataset(const std::string& filename) { loadData(filename); }
+        void loadData(const std::string&);
+        int size() const { return data.size(); }
+        Water operator[](int index) const { return data.at(index); }
+
+    private:
+        std::vector<Water> data;
+        void checkDataExists() const;
+
+};
